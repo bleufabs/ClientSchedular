09e827cd32c3c6e1ab3edf62c1ad076cf0780c7f
bleufabs <bleufabs@gmail.com>
2025-08-06 19:10:58 -0500

    Added files for other frontend pages

--- obj/Debug/net9.0/AndysApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/AndysApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AndysApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d2ede7f635fa01e6bb3e6ee7bfe4af89755b5a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AndysApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AndysApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
